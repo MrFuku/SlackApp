8d13724a8dc2474a6cb4fa34073991e8c47bf2c4
fuku <mmafighter_shun@icloud.com>
2020-01-06 21:29:12 +0900

    インプットフィールド作成

--- pages/index.vue
+++ pages/index.vue
@@ -3,37 +3,22 @@
     <div class="chats-layout">
       <Messages />
     </div>
-        slack-clone-app
-      </h1>
-      <h2 class="subtitle">
-        My phenomenal Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+
+    <div class="input-layout">
+      <ChatForm />
     </div>
   </div>
+  
 </template>
 
 <script>
 import Messages from '~/components/Messages'
+import ChatForm from '~/components/ChatForm'
 
 export default {
   components: {
-    Messages
+    Messages,
+    ChatForm
   }
 }
 </script>
@@ -48,25 +33,7 @@ export default {
   height: 90%;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.input-layout {
+  height: 10%;
 }
 </style>
